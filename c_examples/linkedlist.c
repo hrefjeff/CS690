5a4b4dd8789b2da61b01ba59dd0e32cd51af45c2
Jeffrey Allen <ja0081@uah.edu>
2022-09-12 00:29:45 +0000

    Updated linkedlist example

--- c_examples/linkedlist.c
+++ c_examples/linkedlist.c
@@ -15,11 +15,62 @@ void printLinkedlist(struct node *p) {
     printf("%d ", p->value);
     p = p->next;
   }
+  printf("\n");
+}
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Name      : insertBeginning
+Writer(s) : Jeffrey Allen
+Purpose   : adds a node to the beginning of the list
+Params    : Pointer to a pointer of a node, value
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+void insertBeginning(struct node **head, int val){
+
+  // Allocate mem for new node
+  struct node *newNode;
+  newNode = malloc(sizeof(struct node));
+
+  // Assign vals
+  newNode->value = val;
+  newNode->next = *head;
+
+  // Change the location where head points to
+  *head = newNode;
+}
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Name      : insertEnd
+Writer(s) : Jeffrey Allen
+Purpose   : adds a node to the end of the list
+Params    : Pointer to a node, value
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+void insertEnd(struct node *head, int val){
+
+  // Allocate mem for new node
+  struct node *newNode;
+  newNode = malloc(sizeof(struct node));
+
+  // Assign vals
+  newNode->value = val;
+  newNode->next = NULL;
+
+  // Begin traversal of list and find end
+  struct node *temp = head;
+  while(temp->next != NULL){
+    temp = temp->next;
+  }
+  
+  // Assign the last node's next to newly created node
+  temp->next = newNode;
+}
+
+void delete(struct node *p){
+  
 }
 
 int main() {
   // Initialize nodes
-  struct node *head;
+  struct node *head = NULL;
   struct node *one = NULL;
   struct node *two = NULL;
   struct node *three = NULL;
@@ -41,5 +92,8 @@ int main() {
 
   // printing node-value
   head = one;
+  printLinkedlist(head);
+  insertEnd(head, 5);
+  insertBeginning(&head, 0);
   printLinkedlist(head);
 }
